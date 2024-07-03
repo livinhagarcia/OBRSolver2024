f50ee636c72ecc8cce12d10a14a7ae5128cc3185
PedroDGMalheiros <pdgmalheiros@gmail.com>
2024-07-03 17:08:10 -0300

    Update rescuenv1.py
    lv1 without claw services

--- rescuenv1.py
+++ rescuenv1.py
@@ -10,6 +10,16 @@ u_value = u2.distance()
 
 hub = PrimeHub()
 
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+import umath as math
+
+
+hub = PrimeHub()
+
 class MotorPair:
     def __init__(self, port1, port2):
         self.motor1 = Motor(port1)
@@ -60,12 +70,12 @@ class Robot:
         if force_sensor_port:
             self.force_sensor = ForceSensor(force_sensor_port)
 
-    def pointTo(self, degrees, precision = 5):
+    def pointTo(self, degrees, precision = 3):
         dir = self.position[2]
         # print(range(degrees - precision, degrees + precision))
         if(dir < degrees):
             while self.hub.imu.heading() < (degrees - precision) or self.hub.imu.heading() > (degrees + precision):
-                # print(self.hub.imu.heading())
+                print(self.hub.imu.heading())
                 self.motors.start_tank(-200,200)
             self.motors.stop_tank()
         else:
@@ -75,6 +85,28 @@ class Robot:
         print('turning ' + str(degrees) + ' degrees')
         self.position[2] = self.hub.imu.heading()
         self.map.points[-1][2] = self.position[2]
+    
+    def back_pointTo(self, degrees, precision = 3):
+        dir = self.position[2]
+        if degrees < 0:
+            degrees += 180
+        else:
+            degrees -= 180
+        # degrees = degrees * -1 
+        # print(range(degrees - precision, degrees + precision))
+        if(dir < degrees):
+            while self.hub.imu.heading() < (degrees - precision) or self.hub.imu.heading() > (degrees + precision):
+                print(self.hub.imu.heading())
+                self.motors.start_tank(-200,200)
+            self.motors.stop_tank()
+        else:
+            while self.hub.imu.heading() < (degrees - precision) or self.hub.imu.heading() > (degrees + precision):
+                self.motors.start_tank(200,-200)
+            self.motors.stop_tank()
+        print('turning ' + str(degrees) + ' degrees')
+        self.position[2] = self.hub.imu.heading()
+        self.map.points[-1][2] = self.position[2]
+
 
     def calibrateDirTo(self, goal, precision = 1):
         dir = self.position[2]
@@ -110,10 +142,38 @@ class Robot:
                 self.pointTo(0)
             else:
                 self.pointTo(178)
-            self.motors.move_angle(abs(q), 200, 200)
+            self.motors.move_angle(abs(q),200, 200)
             print('moving ' + str(q) + ' on Y')
             self.position[1] += q
             self.map.addPoint(self.position)
+    
+    def back_goTo(self,x,y):
+        x = int( x + (-1*self.position[0]) )
+        y = int( y + (-1*self.position[1]) )
+        dist = math.sqrt(x**2 + y**2)
+        if x != 0 and y > 0:
+            self.back_pointTo(int(math.degrees(math.atan(x/y))))
+        elif x > 0 and y < 0:
+            self.back_pointTo(90 + int(abs(math.degrees(math.atan(y/x)))))
+        elif x < 0 and y < 0:
+            self.back_pointTo(-90 - int(abs(math.degrees(math.atan(y/x)))))
+        elif x == 0 and y != 0:
+            if y > 0:
+                self.back_pointTo(0)
+            else:
+                self.back_pointTo(180)
+        elif x != 0 and y == 0:
+            if x > 0:
+                self.back_pointTo(90)
+            else:
+                self.back_pointTo(-90)
+        else:
+            return 0
+        self.motors.move_angle(dist,-200, -200)
+        self.position[0] += x
+        self.position[1] += y
+        self.map.addPoint(self.position)
+        return 1
 
     def goTo(self,x,y):
         x = int( x + (-1*self.position[0]) )
@@ -139,25 +199,16 @@ class Robot:
         self.map.addPoint(self.position)
         return 1
 
-    def pointToaPoint(self,x,y):
-        x = int( x + (-1*self.position[0]) )
-        y = int( y + (-1*self.position[1]) )
-        # dist = math.sqrt(x**2 + y**2)
-        if x != 0 and y > 0:
-            self.pointTo(int(math.degrees(math.atan(x/y))))
-        elif x > 0 and y < 0:
-            self.pointTo(90 + int(abs(math.degrees(math.atan(y/x)))))
-        elif x < 0 and y < 0:
-            self.pointTo(-90 - int(abs(math.degrees(math.atan(y/x)))))
-        return 1
-
-    def doRoute(self, pointlist, goandback = 'go'):
+    def doRoute(self, pointlist, goandback = False, back = False):
         for point in pointlist:
-            self.goTo(point[0],point[1])
-        if goandback == 'go_comeback':
+            if back:
+                self.back_goTo(point[0],point[1])    
+            else:
+                self.goTo(point[0],point[1])
+        if goandback == True:
             lista = self.map.points.copy()
             lista.reverse()
-            self.doRoute(lista)
+            self.doRoute(lista, False, back)
 
 def FindSafe(areas):
     
@@ -187,13 +238,14 @@ out = [385,385,-90]
 sc = ColorSensor(Port.D)
 robo = Robot(Port.A, Port.B, None, PontoInicial)
 def main():
-    robo.goTo(1155,1155)#go to center
+    robo.goTo(1155,1155)
     safe = FindSafe(AreaResgate)
     if not safe:
         robo.goTo(out[0], out[1])
         robo.pointTo(out[2])    
     else:
-        robo.goTo(safe[0], safe[1])
-
+        robo.back_goTo(1155,1155)
+        robo.goTo(out[0], out[1])
+        robo.pointTo(out[2])
     print(robo.map.points)
 main()
