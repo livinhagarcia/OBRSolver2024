72cf3d3357a1164a835c49926f669061112a94a7
PedroDGMalheiros <pdgmalheiros@gmail.com>
2024-07-31 17:37:46 -0300

    Create Claw_HubToHub.py

---
+++ Claw_HubToHub.py
@@ -0,0 +1,77 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub(observe_channels=[1])
+
+class MotorPair:
+    def __init__(self, port1, port2):
+        self.motor1 = Motor(port1)
+        self.motor2 = Motor(port2)
+        self.timer = StopWatch()
+    def move_angle(self,amount,speed1,speed2, timeout = 1000):
+        self.motor1.reset_angle(0)
+        self.motor2.reset_angle(0)
+        self.timer.reset()
+        while abs(self.motor1.angle()) < amount or self.timer.time() < timeout:
+            while abs(self.motor2.angle()) < amount or self.timer.time() < timeout:
+                self.motor1.run(speed1)
+                self.motor2.run((speed2))
+        self.motor1.stop()
+        self.motor2.stop()
+        return "succeded"
+    def move_tank(self,amount, speed1, speed2):    
+        self.motor1.run(speed1)
+        self.motor2.run((speed2))
+        wait(amount)
+        self.motor1.stop()
+        self.motor2.stop()
+    def start_tank(self, speed1, speed2):
+        self.motor1.run(speed1)
+        self.motor2.run((speed2))           
+    def stop_tank(self):
+        self.motor1.run(0)
+        self.motor2.run(0)
+
+class Claw:
+    def __init__(self,P1,P2,Hand):
+        self.clawPair = MotorPair(P1,P2)
+        self.hand = Motor(Hand)
+    def up(self):
+        self.clawPair.move_tank(1000,-200,200)
+        print("up")
+    def down(self):
+        self.clawPair.move_tank(700,500,-500)
+        print("down")
+    def open(self):
+        self.hand.run_angle(-500,20)
+    def close(self):
+        self.hand.run_angle(500,30)
+        print("close")
+    def pickUp(self):
+        self.down()
+        wait(1000)
+        self.close()
+        self.up()
+    def release(self):
+        self.open()
+        self.down()
+        wait(1000)
+        self.up()
+
+claw = Claw(Port.A, Port.C, Port.D)
+
+while True:
+    hub.display.pixel(1,1)
+    hub.light.on(Color.RED)
+
+    inf = hub.ble.observe(1)
+    print(inf)
+    if inf == 0:
+        claw.pickUp()
+
+    elif inf == 1:
+        claw.release()
+    inf = None
