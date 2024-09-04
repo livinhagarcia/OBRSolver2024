16af15c935a2e9cb38efb553e816d3d485b51fea
PedroDGMalheiros <pdgmalheiros@gmail.com>
2024-09-04 14:21:45 -0300

    Update Claw_HubToHub.py

--- Claw_HubToHub.py
+++ Claw_HubToHub.py
@@ -4,22 +4,6 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase
 from pybricks.tools import wait, StopWatch
 
-hub = PrimeHub()
-
-from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-
-hub = PrimeHub(broadcast_channel=2, observe_channels=[1])
-
-from pybricks.hubs import PrimeHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-
 hub = PrimeHub(broadcast_channel=2, observe_channels=[1])
 
 class MotorPair:
